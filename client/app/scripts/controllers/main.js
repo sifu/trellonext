a7fe100157da0ae0d54a1867fa42eb8dc8804d0c
Siegmund Führinger <sifu@0xx0.net>
2013-02-26 18:09:29 +0100

    client side only version with real trello authorization

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -4,13 +4,72 @@ angular.module( 'clientApp' )
 .controller( 'MainCtrl', function( $scope, $http ) {
 
   $scope.boardFilter = '';
+  $scope.boards = [];
+  $scope.authorized = false;
 
-  $http.get( '/data' )
-  .then( function( result ) {
-    $scope.boards = result.data;
-    $scope.boards.forEach( function( board ) {
-      board.hasCards = ( board.doingCards.length > 0 ) || ( board.nextCards.length > 0 );
+  var onAuthorize = function( ) {
+    $scope.authorized = true;
+    Trello.get( 'members/me/boards', { lists: 'open' }, function( boards ) {
+      Trello.get( 'members/me/cards/visible', function( cards ) {
+        var result = [];
+        boards.filter( function( b ) { return b.closed === false; } )
+        .forEach( function( board ) {
+
+          var doingList = board.lists.filter( function( list ) {
+            return list.name.toLowerCase( ) === 'doing';
+          } );
+
+          if( doingList.length ) {
+            var idListDoing = doingList[ 0 ].id;
+            var doingCards = cards.filter( function( card ) {
+              return card.idList === idListDoing;
+            } );
+          } else {
+            doingCards = [];
+          }
+
+          var nextList = board.lists.filter( function( list ) {
+            return list.name.toLowerCase( ) === 'next';
+          } );
+
+          if( nextList.length ) {
+            var idListNext = nextList[ 0 ].id;
+            var nextCards = cards.filter( function( card ) {
+              return card.idList === idListNext;
+            } );
+          } else {
+            nextCards = [];
+          }
+
+          var values = {
+            boardName: board.name,
+            boardId: board.id,
+            boardUrl: board.url,
+            nextCards: nextCards,
+            doingCards: doingCards
+          };
+          result.push( values );
+        } );
+        $scope.$apply( function( ) {
+          $scope.boards = result;
+          $scope.boards.forEach( function( board ) {
+            board.hasCards = ( board.doingCards.length > 0 ) || ( board.nextCards.length > 0 );
+          } );
+        } );
+      } );
     } );
+  };
+
+  $scope.authorize = function( ) {
+    Trello.authorize( {
+      type: 'popup',
+      success: onAuthorize
+    } );
+  };
+
+  Trello.authorize( {
+    interactive: false,
+    success: onAuthorize
   } );
 
 } );
